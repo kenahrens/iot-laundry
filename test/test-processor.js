1880a2f39dc5cd91d8655a954c4851eb365514cf
Ken Ahrens <ken.ahrens@gmail.com>
2017-05-28 17:24:23 -0400

    launch travis build

--- test/test-processor.js
+++ test/test-processor.js
@@ -1,7 +1,16 @@
-const processor = require('../lib/processor.js');
+var processor = require('../lib/processor.js');
+var fs = require('fs');
+
+var buffLight;
 
 describe('processor tests', function() {
-  it('reads an image', function(done) {
+  it('reads a test image', function(done) {
+    var fname = './test/images/light-12-orig.jpg';
+    buffLight = fs.readFileSync(fname);
+    done();
+  });
+
+  it('masks the test image', function(done) {
     done();
   });
 })
