06b3c1acf2ab66ae70e92acda750d545655f8b42
Ken Ahrens <ken.ahrens@gmail.com>
2017-05-28 21:34:55 -0400

    adding hashing to test

--- test/test-processor.js
+++ test/test-processor.js
@@ -1,7 +1,11 @@
 var processor = require('../lib/processor.js');
-var fs = require('fs');
 
+var fs = require('fs');
+var assert = require('assert');
+
+var imgLight = '';
 var buffLight;
+var hashLight = 'bj9gsM0800g';
 
 describe('processor tests', function() {
   it('reads a test image', function(done) {
@@ -10,7 +14,10 @@ describe('processor tests', function() {
     done();
   });
 
-  it('masks the test image', function(done) {
-    done();
+  it('hashes the test image', function(done) {
+    processor.hashBuffer(buffLight, function(hash) {
+      assert.equal(hash, hashLight);
+      done();
+    })
   });
 })
